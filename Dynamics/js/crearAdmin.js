862ff08fea810070bb339396445ff620156724ba
José Clemente Cupa Sepúlveda <cupa.sepulveda.jc@gmail.com>
2024-05-05 15:17:32 -0600

    crear y borrar administradores

---
+++ Dynamics/js/crearAdmin.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", ()=>{
+    const crear = document.getElementById("crear");
+    const enviar = document.getElementById("enviar");
+
+    enviar.addEventListener("click",(e)=>{
+        e.preventDefault();
+        datosForm = new FormData(crear);
+        fetch("../Dynamics/php/crearAdmin.php",{
+            method: "POST",
+            body: datosForm,
+        }).then((respuesta)=>{
+            return respuesta.json();
+        }).then((datosJSON)=>{
+            console.log(datosJSON.mensaje);
+            // console.log(datosJSON.usuario);
+            // console.log(datosJSON.contrasena);
+        })
+    })
+});
